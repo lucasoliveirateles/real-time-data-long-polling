4168dae9b5fc932a5d1fe935789613f36790e3bc
Lucas Teles <dev.teles@outlook.com>
2024-05-21 19:13:17 -0300

    add dotenv

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
+import env from 'dotenv';
 import express from 'express';
+
+env.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
